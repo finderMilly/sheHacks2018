344ff65d1264ebcca60ae67e67153a4f61c5a1f6
tarynelise <taryn@taryn.codes>
2018-06-30 13:06:55 +1000

    feat: playing with follow up

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var itemSelection = function (session) {
   //session.send("You said: %s", session.message.text);
   builder.Prompts.choice(
       session,
-      'May I offer one of the following options?',
+      'Sure thing Joyce. Sounds like you could do with some TLC. Did you want to treat yourself with one of the following?',
       options,
       {
           maxRetries: 3,
@@ -45,8 +45,8 @@ var summary = function (session, results) {
   console.log(result);
   builder.Prompts.choice(
     session,
-    `You have selected ${results.response.entity} which costs $${result.price}. We estimate this will be delivered within 30 minutes. Would you like to continue?`,
-    ['Yes', 'No - cancel'],
+    `OK, cool, I'll grab some ${results.response.entity} for you which will cost $${result.price}. We can get it to you within 30 minutes. What do you think?`,
+    ['Yes please', 'No - cancel'],
     {
         maxRetries: 3,
         retryPrompt: "I'm sorry, that's not an option. Please try again!",
@@ -54,35 +54,51 @@ var summary = function (session, results) {
     });
 };
 
-var getPin = function (session, results) {
+var confirmation = function (session, results) {
   console.log('in getPin'+results.response.index);
   var index = results.response.index;
   if (index==1) { //response is cancel
-    session.endDialog(`Okay, we've cancelled your order. Just message us again if you need anything else! We support your decisions! You do you!`);
+    session.endDialog(`OK, we've cancelled your order. Just message us again if you need anything else! We support your decisions! You do you!`);
   }
   if (index == 0) { //response is yes
-    session.endDialog(`Thank you! Your order is on it's way. Don't forget that you're awesome and you can do anything!`);
+    session.send(`Great! Glad to be there for you Joyce. We'll be there for you within 30 minutes.`);
+    // followup(session, results);
   }
-/*
-  var result = store.products.find(product => results.response.entity.indexOf(product.name)>=0);
-  console.log(result);
-  builder.Prompts.choice(
-    session,
-    `You have selected ${results.response.entity} which costs $${result.price}. We estimate this will be delivered within 30 minutes. Would you like to continue?`,
-    ['Yes', 'No - cancel'],
-    {
-        maxRetries: 3,
-        retryPrompt: "I'm sorry, that's not an option. Please try again!",
-        listStyle:builder.ListStyle.button
-    });
-    */
-
 };
+
+// var followup = function (session, results) {
+//   {console.log(results)};
+//   builder.Prompts.choice(
+//     session,
+//      `We know waiting is one of the hardest things to do, so here's something from us in the meantime to keep you going... which would you prefer?`,
+//      ['Ryan Gosling', 'Empowering song', 'Sad girl poetry'],
+//      {
+//          maxRetries: 3,
+//         retryPrompt: "I'm sorry, that's not an option. Please try again!",
+//         listStyle:builder.ListStyle.button
+//     });
+//     gift(session, results);
+//   };
+
+// var gift = function (session, results) {
+//   {console.log(results)};
+//   var index = results.response.index;
+//   if (index==0) { //response is ryan gosling
+//     session.endDialog(`You're going to like this! Just follow this link: https://i.imgflip.com/a6znr.jpg`);
+//   }
+//   if (index == 1) { //response is song
+//     session.endDialog(`You're going to like this! Just follow this link: https://i.imgflip.com/a6znr.jpg`);
+//   }
+//   if (index == 2) { //response is poetry
+//     session.endDialog(`You're going to like this! Just follow this link: https://assets.rbl.ms/10463908/980x.png`);
+//   }
+// };
 
 var bot = new builder.UniversalBot(connector, [
   itemSelection,
   summary,
-  getPin
+  confirmation,
+
 ]);
 
 
